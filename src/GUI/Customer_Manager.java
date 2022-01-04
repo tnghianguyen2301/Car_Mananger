65923667f6f6315d3c89361c4e1672306b7ed620
nhocw <nhocw@PCofNghia>
2022-01-04 21:35:32 +0700

    Add Customer Info

--- src/GUI/Customer_Manager.java
+++ src/GUI/Customer_Manager.java
@@ -8,18 +8,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Font;
 
 public class Customer_Manager extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JTextField Cus_Name;
+	private JTextField Cus_Birth;
+	private JTextField Cus_Address;
+	private JTextField Cus_Phone;
 
 	/**
 	 * Launch the application.
@@ -43,7 +49,7 @@ public class Customer_Manager extends JFrame {
 	public Customer_Manager() {
 		setTitle("Customer Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 400);
+		setBounds(100, 100, 600, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -75,6 +81,116 @@ public class Customer_Manager extends JFrame {
 	    JButton Edit_Cus_Btn = new JButton("Edit Profile");
 	    Edit_Cus_Btn.setFont(new Font("Tahoma", Font.BOLD, 20));
 	    Bot_Pane.add(Edit_Cus_Btn);
+	    
+	    //Infomation Pane
+	    JPanel Sale_Pane = new JPanel();
+	    contentPane.add(Sale_Pane, BorderLayout.EAST);
+	    GridBagLayout gbl_Sale_Pane = new GridBagLayout();
+	    gbl_Sale_Pane.columnWidths = new int[]{0, 0};
+	    gbl_Sale_Pane.rowHeights = new int[]{0, 0};
+	    gbl_Sale_Pane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+	    gbl_Sale_Pane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+	    Sale_Pane.setLayout(gbl_Sale_Pane);
+	    
+	    JPanel panel = new JPanel();
+	    GridBagConstraints gbc_panel = new GridBagConstraints();
+	    gbc_panel.insets = new Insets(0, 5, 5, 5);
+	    gbc_panel.fill = GridBagConstraints.BOTH;
+	    gbc_panel.gridx = 0;
+	    gbc_panel.gridy = 0;
+	    Sale_Pane.add(panel, gbc_panel);
+	    GridBagLayout gbl_panel = new GridBagLayout();
+	    gbl_panel.columnWidths = new int[] {80, 80};
+	    gbl_panel.rowHeights = new int[] {30, 20, 30, 20, 30, 20, 30, 20, 30};
+	    gbl_panel.columnWeights = new double[]{0.0, 0.0};
+	    gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	    panel.setLayout(gbl_panel);
+	    
+	    JLabel lblNewLabel_1 = new JLabel("Customer Infomation:");
+	    lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+	    GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+	    gbc_lblNewLabel_1.fill = GridBagConstraints.VERTICAL;
+	    gbc_lblNewLabel_1.gridwidth = 2;
+	    gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+	    gbc_lblNewLabel_1.gridx = 0;
+	    gbc_lblNewLabel_1.gridy = 0;
+	    panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+	    
+	    JLabel lblNewLabel_2 = new JLabel("Customer Name:");
+	    GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+	    gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+	    gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
+	    gbc_lblNewLabel_2.gridx = 0;
+	    gbc_lblNewLabel_2.gridy = 1;
+	    panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+	    
+	    Cus_Name = new JTextField();
+	    Cus_Name.setFont(new Font("Tahoma", Font.BOLD, 11));
+	    GridBagConstraints gbc_Cus_Name = new GridBagConstraints();
+	    gbc_Cus_Name.gridwidth = 2;
+	    gbc_Cus_Name.fill = GridBagConstraints.BOTH;
+	    gbc_Cus_Name.insets = new Insets(0, 0, 5, 0);
+	    gbc_Cus_Name.gridx = 0;
+	    gbc_Cus_Name.gridy = 2;
+	    panel.add(Cus_Name, gbc_Cus_Name);
+	    Cus_Name.setColumns(10);
+	    
+	    JLabel lblNewLabel_2_1 = new JLabel("Date of birth:");
+	    GridBagConstraints gbc_lblNewLabel_2_1 = new GridBagConstraints();
+	    gbc_lblNewLabel_2_1.anchor = GridBagConstraints.WEST;
+	    gbc_lblNewLabel_2_1.insets = new Insets(0, 0, 0, 5);
+	    gbc_lblNewLabel_2_1.gridx = 0;
+	    gbc_lblNewLabel_2_1.gridy = 3;
+	    panel.add(lblNewLabel_2_1, gbc_lblNewLabel_2_1);
+	    
+	    Cus_Birth = new JTextField();
+	    Cus_Birth.setFont(new Font("Tahoma", Font.BOLD, 11));
+	    Cus_Birth.setColumns(10);
+	    GridBagConstraints gbc_Cus_Birth = new GridBagConstraints();
+	    gbc_Cus_Birth.gridwidth = 2;
+	    gbc_Cus_Birth.fill = GridBagConstraints.BOTH;
+	    gbc_Cus_Birth.insets = new Insets(0, 0, 5, 0);
+	    gbc_Cus_Birth.gridx = 0;
+	    gbc_Cus_Birth.gridy = 4;
+	    panel.add(Cus_Birth, gbc_Cus_Birth);
+	    
+	    JLabel lblNewLabel_2_2 = new JLabel("Address:");
+	    GridBagConstraints gbc_lblNewLabel_2_2 = new GridBagConstraints();
+	    gbc_lblNewLabel_2_2.anchor = GridBagConstraints.WEST;
+	    gbc_lblNewLabel_2_2.insets = new Insets(0, 0, 0, 5);
+	    gbc_lblNewLabel_2_2.gridx = 0;
+	    gbc_lblNewLabel_2_2.gridy = 5;
+	    panel.add(lblNewLabel_2_2, gbc_lblNewLabel_2_2);
+	    
+	    Cus_Address = new JTextField();
+	    Cus_Address.setFont(new Font("Tahoma", Font.BOLD, 11));
+	    Cus_Address.setColumns(10);
+	    GridBagConstraints gbc_Cus_Address = new GridBagConstraints();
+	    gbc_Cus_Address.gridwidth = 2;
+	    gbc_Cus_Address.fill = GridBagConstraints.BOTH;
+	    gbc_Cus_Address.insets = new Insets(0, 0, 5, 0);
+	    gbc_Cus_Address.gridx = 0;
+	    gbc_Cus_Address.gridy = 6;
+	    panel.add(Cus_Address, gbc_Cus_Address);
+	    
+	    JLabel lblNewLabel_2_2_1 = new JLabel("Phone number:");
+	    GridBagConstraints gbc_lblNewLabel_2_2_1 = new GridBagConstraints();
+	    gbc_lblNewLabel_2_2_1.anchor = GridBagConstraints.WEST;
+	    gbc_lblNewLabel_2_2_1.insets = new Insets(0, 0, 0, 5);
+	    gbc_lblNewLabel_2_2_1.gridx = 0;
+	    gbc_lblNewLabel_2_2_1.gridy = 7;
+	    panel.add(lblNewLabel_2_2_1, gbc_lblNewLabel_2_2_1);
+	    
+	    Cus_Phone = new JTextField();
+	    Cus_Phone.setFont(new Font("Tahoma", Font.BOLD, 11));
+	    Cus_Phone.setColumns(10);
+	    GridBagConstraints gbc_Cus_Phone = new GridBagConstraints();
+	    gbc_Cus_Phone.gridwidth = 2;
+	    gbc_Cus_Phone.fill = GridBagConstraints.BOTH;
+	    gbc_Cus_Phone.insets = new Insets(0, 0, 5, 0);
+	    gbc_Cus_Phone.gridx = 0;
+	    gbc_Cus_Phone.gridy = 8;
+	    panel.add(Cus_Phone, gbc_Cus_Phone);
 	}
 
 }
