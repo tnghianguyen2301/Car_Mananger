392c6c00f9d52fc8fd1269afbe21eac5befcee63
Leducthanh1 <ronthanhyal@gmail.com>
2022-01-07 18:59:28 +0700

    update 7-1

--- src/GUI/Manage.java
+++ src/GUI/Manage.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,8 +27,6 @@ import DAO.CarProductsModel;
 import DAO.HistoryProdutcsDAO;
 import DTO.User;
 import Untilities.DBConnection;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
 
 public class Manage extends JFrame {
 
@@ -65,7 +62,7 @@ public class Manage extends JFrame {
 	public Manage() throws ClassNotFoundException, IOException, SQLException {
 		setTitle("Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 510);
+		setBounds(100, 100, 825, 510);
 		Manage_Interface = new JPanel();
 		Manage_Interface.setBorder(new EmptyBorder(5, 5, 5, 5));
 		Manage_Interface.setLayout(new BorderLayout(0, 0));
@@ -74,6 +71,7 @@ public class Manage extends JFrame {
 		
 		JPanel Main_Manage_Pane = new JPanel();
 		Manage_Interface.add(Main_Manage_Pane, BorderLayout.CENTER);
+		Main_Manage_Pane.setLayout(null);
 		
 		//Data Table
 		  DBConnection.init("database.properties");
@@ -83,13 +81,13 @@ public class Manage extends JFrame {
 		  hpDAO = new HistoryProdutcsDAO(conn);
 		  hpDAO.loadHistoryCarDataToList();
 		  tableModel = new CarHistoryModel(hpDAO);
-	      Main_Manage_Pane.setLayout(new GridLayout(0, 1, 0, 0));
 	      table = new JTable(tableModel);
-	      table.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+	      table.setFont(new Font("Times New Roman", Font.PLAIN, 10));
 	      table.setAutoCreateRowSorter(true);
 	      JScrollPane scrollPane = new JScrollPane(table);
+	      scrollPane.setBounds(0, 0, 800, 400);
 	      scrollPane.setPreferredSize(new Dimension(800, 400));
-	      Main_Manage_Pane.add(scrollPane);
+	      Main_Manage_Pane.add(scrollPane, BorderLayout.CENTER);
 		
 	    //Bot Pane
 	      JPanel Bot_Manage_Pane = new JPanel();
@@ -102,10 +100,6 @@ public class Manage extends JFrame {
 	      Bot_Manage_Pane.setLayout(gbl_Bot_Manage_Pane);
 		
 	      	JButton Add_Pd_Btn = new JButton("Add Product");
-	      	Add_Pd_Btn.addActionListener(new ActionListener() {
-	      		public void actionPerformed(ActionEvent e) {
-	      		}
-	      	});
 			Add_Pd_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
 			GridBagConstraints gbc_Add_Pd_Btn = new GridBagConstraints();
 			gbc_Add_Pd_Btn.fill = GridBagConstraints.BOTH;
@@ -113,14 +107,8 @@ public class Manage extends JFrame {
 			gbc_Add_Pd_Btn.gridx = 0;
 			gbc_Add_Pd_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Add_Pd_Btn, gbc_Add_Pd_Btn);
-			ImageIcon Add_Pd_Icon = new ImageIcon(this.getClass().getResource("/Add_Icon.png"));
-			Add_Pd_Btn.setIcon(Add_Pd_Icon);
 			
 			JButton Add_Acc_Btn = new JButton("Manage Account");
-			Add_Acc_Btn.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
 			Add_Acc_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
 			GridBagConstraints gbc_Add_Acc_Btn = new GridBagConstraints();
 			gbc_Add_Acc_Btn.fill = GridBagConstraints.BOTH;
@@ -128,8 +116,6 @@ public class Manage extends JFrame {
 			gbc_Add_Acc_Btn.gridx = 1;
 			gbc_Add_Acc_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Add_Acc_Btn, gbc_Add_Acc_Btn);
-			ImageIcon User_Icon = new ImageIcon(this.getClass().getResource("/User_Icon.png"));
-			Add_Acc_Btn.setIcon(User_Icon);
 		
 			JButton Man_Cus_Btn = new JButton("Manage Customer");
 			Man_Cus_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -139,14 +125,8 @@ public class Manage extends JFrame {
 			gbc_Man_Cus_Btn.gridx = 2;
 			gbc_Man_Cus_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Man_Cus_Btn, gbc_Man_Cus_Btn);
-			ImageIcon Add_Cus_Icon = new ImageIcon(this.getClass().getResource("/Customer_Icon.png"));
-			Man_Cus_Btn.setIcon(Add_Cus_Icon);
 		
 			JButton Rep_Car_Btn = new JButton("Report Car Type");
-			Rep_Car_Btn.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
 			Rep_Car_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
 			GridBagConstraints gbc_Rep_Car_Btn = new GridBagConstraints();
 			gbc_Rep_Car_Btn.fill = GridBagConstraints.BOTH;
@@ -154,22 +134,14 @@ public class Manage extends JFrame {
 			gbc_Rep_Car_Btn.gridx = 3;
 			gbc_Rep_Car_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Rep_Car_Btn, gbc_Rep_Car_Btn);
-			ImageIcon Car_Icon = new ImageIcon(this.getClass().getResource("/Car_Icon.png"));
-			Rep_Car_Btn.setIcon(Car_Icon);
 		
 			JButton Rep_Tur_Btn = new JButton("Report Turnover");
-			Rep_Tur_Btn.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
 			Rep_Tur_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
 			GridBagConstraints gbc_Rep_Tur_Btn = new GridBagConstraints();
 			gbc_Rep_Tur_Btn.fill = GridBagConstraints.BOTH;
 			gbc_Rep_Tur_Btn.gridx = 4;
 			gbc_Rep_Tur_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Rep_Tur_Btn, gbc_Rep_Tur_Btn);
-			ImageIcon Turnover_Icon = new ImageIcon(this.getClass().getResource("/Report_Icon.png"));
-			Rep_Tur_Btn.setIcon(Turnover_Icon);
 		
 		//Top Pane
 		JPanel Top_Manage_Pane = new JPanel();
@@ -182,18 +154,6 @@ public class Manage extends JFrame {
 		
 		JButton Back_Main_Interface = new JButton("Back");
 		Top_Manage_Pane.add(Back_Main_Interface, BorderLayout.EAST);
-        ImageIcon Back_Icon = new ImageIcon(this.getClass().getResource("/Back_Icon.png"));
-        Back_Main_Interface.setIcon(Back_Icon);
-		
-		JPanel Offset_Top = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) Offset_Top.getLayout();
-		flowLayout.setVgap(3);
-		Top_Manage_Pane.add(Offset_Top, BorderLayout.NORTH);
-		
-		JPanel Offset_Bot = new JPanel();
-		FlowLayout flowLayout_1 = (FlowLayout) Offset_Bot.getLayout();
-		flowLayout_1.setVgap(3);
-		Top_Manage_Pane.add(Offset_Bot, BorderLayout.SOUTH);
 	    this.setPreferredSize(new Dimension(800, 600));
 	    
 	    Back_Main_Interface.addActionListener(new ActionListener() {
@@ -222,7 +182,7 @@ public class Manage extends JFrame {
 	public Manage(User user) throws ClassNotFoundException, IOException, SQLException {
 		setTitle("Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 510);
+		setBounds(100, 100, 825, 510);
 		Manage_Interface = new JPanel();
 		Manage_Interface.setBorder(new EmptyBorder(5, 5, 5, 5));
 		Manage_Interface.setLayout(new BorderLayout(0, 0));
@@ -242,13 +202,8 @@ public class Manage extends JFrame {
 		  hpDAO = new HistoryProdutcsDAO(conn);
 		  hpDAO.loadHistoryCarDataToList();
 		  tableModel = new CarHistoryModel(hpDAO);
-	      Main_Manage_Pane.setLayout(new GridLayout(0, 1, 0, 0));
 	      table = new JTable(tableModel);
-<<<<<<< HEAD
 	      table.setFont(new Font("Constantia", Font.PLAIN, 15));
-=======
-	      table.setFont(new Font("Times New Roman", Font.PLAIN, 15));
->>>>>>> branch 'master' of https://github.com/nhocwhy05/Car_Mananger
 	      table.setAutoCreateRowSorter(true);
 	      JScrollPane scrollPane = new JScrollPane(table);
 	      scrollPane.setBounds(0, 0, 800, 400);
@@ -273,8 +228,6 @@ public class Manage extends JFrame {
 			gbc_Add_Pd_Btn.gridx = 0;
 			gbc_Add_Pd_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Add_Pd_Btn, gbc_Add_Pd_Btn);
-			ImageIcon Add_Pd_Icon = new ImageIcon(this.getClass().getResource("/Add_Icon.png"));
-			Add_Pd_Btn.setIcon(Add_Pd_Icon);
 			
 			JButton Add_Acc_Btn = new JButton("Manage Staff");
 			Add_Acc_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -284,8 +237,6 @@ public class Manage extends JFrame {
 			gbc_Add_Acc_Btn.gridx = 1;
 			gbc_Add_Acc_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Add_Acc_Btn, gbc_Add_Acc_Btn);
-			ImageIcon User_Icon = new ImageIcon(this.getClass().getResource("/User_Icon.png"));
-			Add_Acc_Btn.setIcon(User_Icon);
 		
 			JButton Man_Cus_Btn = new JButton("Manage Customer");
 			Man_Cus_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -295,8 +246,6 @@ public class Manage extends JFrame {
 			gbc_Man_Cus_Btn.gridx = 2;
 			gbc_Man_Cus_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Man_Cus_Btn, gbc_Man_Cus_Btn);
-			ImageIcon Add_Cus_Icon = new ImageIcon(this.getClass().getResource("/Customer_Icon.png"));
-			Man_Cus_Btn.setIcon(Add_Cus_Icon);
 		
 			JButton Rep_Car_Btn = new JButton("Report Car Type");
 			Rep_Car_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -306,8 +255,6 @@ public class Manage extends JFrame {
 			gbc_Rep_Car_Btn.gridx = 3;
 			gbc_Rep_Car_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Rep_Car_Btn, gbc_Rep_Car_Btn);
-			ImageIcon Car_Icon = new ImageIcon(this.getClass().getResource("/Car_Icon.png"));
-			Rep_Car_Btn.setIcon(Car_Icon);
 		
 			JButton Rep_Tur_Btn = new JButton("Report Turnover");
 			Rep_Tur_Btn.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -316,8 +263,6 @@ public class Manage extends JFrame {
 			gbc_Rep_Tur_Btn.gridx = 4;
 			gbc_Rep_Tur_Btn.gridy = 0;
 			Bot_Manage_Pane.add(Rep_Tur_Btn, gbc_Rep_Tur_Btn);
-			ImageIcon Turnover_Icon = new ImageIcon(this.getClass().getResource("/Report_Icon.png"));
-			Rep_Tur_Btn.setIcon(Turnover_Icon);
 		
 		//Top Pane
 		JPanel Top_Manage_Pane = new JPanel();
@@ -330,19 +275,6 @@ public class Manage extends JFrame {
 		
 		JButton Back_Main_Interface = new JButton("Back");
 		Top_Manage_Pane.add(Back_Main_Interface, BorderLayout.EAST);
-		ImageIcon Back_Icon = new ImageIcon(this.getClass().getResource("/Back_Icon.png"));
-        Back_Main_Interface.setIcon(Back_Icon);
-	    this.setPreferredSize(new Dimension(800, 600));
-	    
-	    JPanel Offset_Top = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) Offset_Top.getLayout();
-		flowLayout.setVgap(3);
-		Top_Manage_Pane.add(Offset_Top, BorderLayout.NORTH);
-		
-		JPanel Offset_Bot = new JPanel();
-		FlowLayout flowLayout_1 = (FlowLayout) Offset_Bot.getLayout();
-		flowLayout_1.setVgap(3);
-		Top_Manage_Pane.add(Offset_Bot, BorderLayout.SOUTH);
 	    this.setPreferredSize(new Dimension(800, 600));
 	    
 	    Back_Main_Interface.addActionListener(new ActionListener() {
