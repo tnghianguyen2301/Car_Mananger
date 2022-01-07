b824786af4c14c7df3ab44367f0dc69524b63cfd
nhocw <nhocw@PCofNghia>
2022-01-07 19:39:06 +0700

    test cm

--- src/GUI/Application.java
+++ src/GUI/Application.java
@@ -93,7 +93,7 @@ public class Application extends JFrame {
 		  cpDAO.loadDataCarToList();
 		  tableModel = new CarProductsModel(cpDAO);
 	      table = new JTable(tableModel);
-	      table.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+	      table.setFont(new Font("Times New Roman", Font.PLAIN, 15));
 	      table.setAutoCreateRowSorter(true);
 	      JScrollPane scrollPane = new JScrollPane(table);
 	      scrollPane.setBounds(0, 0, 800, 400);
